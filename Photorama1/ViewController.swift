2840addab30fccb830d5286d25fa4f6b074fc0f8
Partha <the.saintneo@gmail.com>
2020-05-23 19:25:47 -0400

    Initial Commit

---
+++ Photorama1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama1
+//
+//  Created by Partha Sarathy on 5/23/20.
+//  Copyright © 2020 Partha Sarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
